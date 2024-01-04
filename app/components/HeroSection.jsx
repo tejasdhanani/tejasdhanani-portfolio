9d7fc62d71f7c9f609cf3b4379b63bf17e190226
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-03 21:05:36 -0800

    react-type-animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image"
+import { TypeAnimation } from "react-type-animation"
 
 const HeroSection = () => {
   return (
@@ -7,7 +9,23 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl  font-extrabold">
             <span class="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">Hello, I'm{" "}</span>
-            Tejas
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Tejas',
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Software Developer',
+                1000,
+                'Web Developer',
+                1000,
+                'Learner',
+                1000
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-4">
             loremkdshf ksdjfh gkjdsh fgkdsh fgkdshf gkhdsfkghsdfkjghdsfk jghsdkfhgksdfh gksdfhg
