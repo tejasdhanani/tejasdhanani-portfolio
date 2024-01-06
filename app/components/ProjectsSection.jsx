4ea001fe39f7246ed226ffdd0d53f8728b605879
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-05 22:35:50 -0800

    basic animation of home page

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,7 +1,8 @@
 "use client"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import ProjectCard from "./ProjectCard"
 import ProjectTag from "./ProjectTag"
+import { motion, useInView } from "framer-motion"
 
 const projectsData = [
   {
@@ -35,6 +36,9 @@ const projectsData = [
 
 
 const ProjectsSection = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const [tag, setTag] = useState("All");
 
   const handleTagChange = (newTag) => {
@@ -43,8 +47,13 @@ const ProjectsSection = () => {
 
   const filteredProjects = projectsData.filter((project) => project.tag.includes(tag));
 
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 }
+  }
+
   return (
-    <>
+    <section>
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
@@ -53,10 +62,20 @@ const ProjectsSection = () => {
         <ProjectTag onClick={handleTagChange} name="Web" isSelected={tag === "Web"} />
         <ProjectTag onClick={handleTagChange} name="Mobile" isSelected={tag === "Mobile"} />
       </div>
-      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl} />)}
-      </div>
-    </>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) =>
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl} />
+          </motion.li>
+        )}
+      </ul>
+    </section>
   )
 }
 export default ProjectsSection
