ed4c23bf754a56ba350c146f2f017baa1bc4400c
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-05 22:57:06 -0800

    fixed navbar links to it's components

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -56,7 +56,7 @@ const AboutSection = () => {
     }
 
     return (
-        <section className="text-white">
+        <section id="about" className="text-white">
             <div className="md: grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
                 <Image priority src="/about-image.png" width={500} height={500} alt="about-image" />
                 <div className="mt-4 md:mt-0 text-left flex-col h-full">
