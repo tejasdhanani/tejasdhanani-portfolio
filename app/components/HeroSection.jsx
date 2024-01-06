7f413016b7c3aa4654ee92c201b3c689e4d48d43
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-05 22:46:09 -0800

    quick fix in HeroSection

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,7 +1,9 @@
 "use client";
-import Image from "next/image"
-import { TypeAnimation } from "react-type-animation"
-import { motion } from "framer-motion"
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -13,40 +15,46 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal  font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I'm{" "}</span>
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&apos;m{" "}
+            </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                // Same substring at the start will only be typed out once, initially
-                'Tejas',
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                'Software Developer',
+                "Tejas",
                 1000,
-                'Web Developer',
+                "Web Developer",
                 1000,
-                'Learner',
-                1000
+                "Software Developer",
+                1000,
+                "Learner",
+                1000,
               ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-4">
-            loremkdshf ksdjfh gkjdsh fgkdsh fgkdshf gkhdsfkghsdfkjghdsfk jghsdkfhgksdfh gksdfhg
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
           </p>
           <div>
-            <button
+            <Link
+              href="/#contact"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
-            </button>
-            <button
+            </Link>
+            <Link
+              href="/"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
-            </button>
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
           </div>
         </motion.div>
         <motion.div
@@ -57,7 +65,6 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              priority
               src="/hero-image.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
@@ -67,7 +74,8 @@ const HeroSection = () => {
           </div>
         </motion.div>
       </div>
-    </section >
-  )
-}
-export default HeroSection
+    </section>
+  );
+};
+
+export default HeroSection;
