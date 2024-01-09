074e556b889a269fc3b1d4a80f95bf4b090a9236
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-08 23:03:53 -0800

    frontend posting email req

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -10,33 +10,24 @@ const EmailSection = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            subject: e.target.subject.value,
-            message: e.target.message.value,
-        };
-        const JSONdata = JSON.stringify(data);
-        const endpoint = "/api/send";
 
-        // Form the request for sending data to the server.
-        const options = {
-            // The method is POST because we are sending data.
+        const response = await fetch("/api/sendemail", {
             method: "POST",
-            // Tell the server we're sending JSON.
             headers: {
                 "Content-Type": "application/json",
             },
-            // Body of the request is the JSON data we created above.
-            body: JSONdata,
-        };
+            body: JSON.stringify({
+                email: e.target.email.value,
+                subject: e.target.subject.value,
+                message: e.target.message.value,
+            }),
+        });
 
-        // const response = await fetch(endpoint, options);
         // const resData = await response.json();
-
-        // if (response.status === 200) {
-        console.log("Message sent.");
-        setEmailSubmitted(true);
-        // }
+        if (response.status === 200) {
+            console.log("Message sent.");
+            setEmailSubmitted(true);
+        }
     };
 
     return (
