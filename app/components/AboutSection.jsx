f4f32b9daa929e0d4ef0ee5020c28e70f9d4d143
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-08 22:15:20 -0800

    updated the about section

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -9,11 +9,14 @@ const TAB_DATA = [
         id: "skills",
         content: (
             <ul className="list-disc pl-2">
-                <li>Node.js</li>
-                <li>Node.js</li>
-                <li>Node.js</li>
-                <li>Node.js</li>
-                <li>Node.js</li>
+                <li>NEXT.js</li>
+                <li>React</li>
+                <li>JavaScript</li>
+                <li>Java</li>
+                <li>PHP</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Tailwind CSS</li>
             </ul>
         )
     },
@@ -22,11 +25,8 @@ const TAB_DATA = [
         id: "education",
         content: (
             <ul>
-                <li>eduu.js</li>
-                <li>Node.js</li>
-                <li>Node.js</li>
-                <li>Node.js</li>
-                <li>Node.js</li>
+                <li>I earned my Bachelor of Science in Information Technology from Fairleigh Dickinson University in Vancouver, BC, Canada, graduating in 2022 with a GPA of 3.7 out of 4.</li>
+
             </ul>
         )
     },
@@ -62,15 +62,16 @@ const AboutSection = () => {
                 <div className="mt-4 md:mt-0 text-left flex-col h-full">
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
-                        lksdfg kldshf gkjsdhfkjg hdksjfhgkjdsfh gkdsjfhgkjds hf kghdsfk ghd
-                        dskjfghdskfjghdslkjffghkdsjfhgkjdshgkdshfgkdshfkgh dsfk gjhdskfghdskfjg
-                        dsfgkjdhsfkj ghdksjfhgkjdsfhgkdhfgkdjshgkdsjhgkdshfgldjsf;lgjdsflkgjdlskfg
-                        kjdslkfg jdlskfjgkl;sdjfglkd;sfgjsdkl;fgj
+                        Hey, I'm Tejas Dhanani, a tech enthusiast with a degree in Information Technology and 1.5 years of hands-on coding experience.
+                        I love turning ideas into user-friendly software, focusing on Next.js, Javascript, Node.js, and MongoDB.
+                        Beyond coding, I'm exploring the latest tech trends, always on the lookout for that next 'aha' moment.
+                        If you're after a developer who blends technical skills with creativity, let's connect.
+                        I'm up for coding challenges and discussions on the future of tech. Let's build something awesome together!
                     </p>
                     <div className="flex flex-row justify-start mt-8">
                         <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>Skills</TabButton>
                         <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>Education</TabButton>
-                        <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>Certifications</TabButton>
+                        {/* <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>Certifications</TabButton> */}
                     </div>
                     <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
                 </div>
