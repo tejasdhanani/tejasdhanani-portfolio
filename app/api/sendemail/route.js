327c463a024789d81875340fab35e88dec661522
Tejas Dhanani <tejas27dhanani@gmail.com>
2024-01-09 17:29:48 -0800

    configured nodemailer gmail port and host

--- app/api/sendemail/route.js
+++ app/api/sendemail/route.js
@@ -5,24 +5,13 @@ export async function POST(request) {
     const { email, subject, message } = await request.json();
 
     var transporter = nodemailer.createTransport({
-        service: "gmail",
+        port: 465,
+        host: "smtp.gmail.com",
         auth: {
             user: process.env.NODEMAILER_EMAIL,
             pass: process.env.NODEMAILER_PW,
         },
-    });
-
-    await new Promise((resolve, reject) => {
-        // verify connection configuration
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
-        });
+        secure: true
     });
 
     var mailOptions = {
@@ -32,29 +21,16 @@ export async function POST(request) {
         text: `Sent by: ${email}\n\n${message}`,
     };
 
-    await new Promise((resolve, reject) => {
-        // send mail
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                console.log("Email Sent");
 
-                console.log(info);
-                resolve(info);
-            }
-        });
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log("Email Sent");
+            console.log(info);
+        }
     });
-
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         throw new Error(error);
-    //     } else {
-    //         console.log("Email Sent");
-    //         return true;
-    //     }
-    // });
 
     return Response.json({
         message: `email sent to ${email}`
